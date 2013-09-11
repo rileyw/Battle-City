ced0c357207076791b5d2a0bc872a4a21b6d108e
Rileyw <riley.wills@acu.edu>
2013-09-11 15:07:00 -0500

    Moved files o.O...

--- battle-city/win_main.cpp
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    return EXIT_SUCCESS;
-}
---
+++ win_main.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+/// <summary>
+/// Windows 32 main entry point
+/// </summary>
+/// <param name="hInstance">Window instance</param>
+/// <param name="hPreviousInstance">Previous window instance</param>
+/// <param name="lpCmdLine">Command line arguments</param>
+/// <param name="nCmdShow">The asynchronous command show.</param>
+/// <returns>0</returns>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreviousInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    return EXIT_SUCCESS;
+}
